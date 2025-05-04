090539755516c71dc9350163894f56094ae55d99
giusmili <giusmili67@gmail.com>
2025-05-04 17:41:20 +0200

    class

--- model/bdd.php
+++ model/bdd.php
@@ -1,13 +1,40 @@
 <?php
 # 1. Connexion à la base de données
-    $host = 'localhost';
-    $db   = 'abonnement';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ];
-    $pdo = new PDO($dsn, $user, $pass, $options);
+/* 
+$host = 'localhost';
+$db   = 'ton_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+*/
+
+class Connexion{
+    public function bdd(){
+
+        $host = 'localhost';
+        $db   = 'abonnement';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+        
+        return $pdo = new PDO($dsn, $user, $pass, $options);
+    
+    }
+}
+
+$newBDD = new Connexion();
+
+$pdo = $newBDD->bdd();
+   
